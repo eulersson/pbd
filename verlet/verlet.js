f924a9879d46746dfa3b59df65b9a8fd651bb2e3
Ramon Blanquer <blanquer.ramon@gmail.com>
2017-03-06 11:58:18 +0000

    WIP fixed 'this' problem

--- verlet/verlet.js
+++ verlet/verlet.js
@@ -44,12 +44,39 @@ ParticleSystem.prototype.initializeGL = function() {
 
   var success = gl.getProgramParameter(this.program, gl.LINK_STATUS);
   if (!success) {
-    console.error(gl.getProgramInfoLog(this.program));
+    console.log(gl.getProgramInfoLog(this.program));
   }
 
-  this.resolutionUniformLocation = gl.getUniformLocation(this.program, "resolution");
-  this.positionAttributeLocation = gl.getAttribLocation(this.program, "position");
+  this.positionAttributeLocation = gl.getAttribLocation(this.program, 'position');
+  this.resolutionUniformLocation = gl.getUniformLocation(this.program, 'resolution');
+
   this.positionBuffer = gl.createBuffer();
+
+  var positions = [
+     30.0, 350.0,
+     80.0, 350.0,
+    500.0, 350.0,
+  ];
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  gl.viewport(0, 0, this.w, this.h);
+}
+
+// Draw scene
+ParticleSystem.prototype.draw = function() {
+  gl.clearColor(0.23, 0.29, 0.25, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(this.program);
+  gl.enableVertexAttribArray(this.positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+
+  gl.vertexAttribPointer(this.positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.uniform2f(this.resolutionUniformLocation, this.w, this.h);
+
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
 
 // Accumulates forces for each particle
@@ -69,33 +96,6 @@ ParticleSystem.prototype.satisfyConstraints = function() {
 
 }
 
-// Draw scene
-ParticleSystem.prototype.draw = function() {
-  gl.clearColor(0.23, 0.29, 0.25, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-
-  var positions = [
-     30.0, 350.0,
-     80.0, 350.0,
-    500.0, 350.0,
-  ];
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  gl.useProgram(this.program);
-  console.log(this.w);
-  console.log(this.h);
-  gl.uniform2f(this.resolutionUniformLocation, this.w, this.h);
-  gl.enableVertexAttribArray(this.positionAttributeLocation);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-
-  gl.vertexAttribPointer(this.positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.drawArrays(gl.POINTS, 0, 3);
-}
-
 // Perform a substep
 ParticleSystem.prototype.timeStep = function() {
   this.accumulateForces();
@@ -105,4 +105,12 @@ ParticleSystem.prototype.timeStep = function() {
 }
 
 var ps = new ParticleSystem('screen');
-setInterval(ps.draw, 1000);
+setInterval(ps.draw.bind(ps), 3000);
+
+/*
+WebGL: INVALID_OPERATION: vertexAttribPointer: no bound ARRAY_BUFFER
+ParticleSystem.draw @ verlet.js:95
+verlet.js:96 WebGL: INVALID_OPERATION: drawArrays: no valid shader program in use
+ParticleSystem.draw @ verlet.js:96
+verlet.js:85 WebGL: INVALID_OPERATION: bufferData: no buffer
+*/
