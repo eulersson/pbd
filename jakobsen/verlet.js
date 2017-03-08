0c8f3dd936e6a91448cc79e273f7debfdd6452da
Ramon Blanquer <blanquer.ramon@gmail.com>
2017-03-08 17:22:59 +0000

    Improved documentation

--- jakobsen/verlet.js
+++ jakobsen/verlet.js
@@ -3,6 +3,10 @@ var gl;
 /** 
  * Helper function that compiles a shader.
  * @function
+ * @param {object} gl WebGL context extracted from the canvas.
+ * @param {enum} type Can be gl.VERTEX_SHADER or gl.FRAGMENT_SHADER.
+ * @param {string} source GLSL text to compile.
+ * @returns {object} Compiled shader.
  */
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -34,6 +38,9 @@ function PinConstraint(A, x, y) {
  * Adjusts the position of the particle associated with the constraint in
  * order to satisfy it. This method will get called in the
  * {@link ParticleSystem#satisfyConstraints}.
+ * @param {list} curPositions The current list of particles so it can be modified.
+ * @returns {boolean} If the constraint is still alive, in oder words if it does
+ * not have to be deleted.
  */
 PinConstraint.prototype.project = function(curPositions) {
   curPositions[this.A] = { x: this.x, y: this.y };
@@ -63,13 +70,15 @@ function SpringConstraint(A, B, restLength, stiffness) {
  * Adjusts the positions of the particles associated with the constraint in
  * order to satisfy it. This method will get called in the
  * {@link ParticleSystem#satisfyConstraints}.
+ * @param {list} curPositions The current list of particles so it can be modified.
+ * @returns {boolean} If the constraint is still alive, in oder words if it does
+ * not have to be deleted.
  */
 SpringConstraint.prototype.project = function (curPositions) { 
   var pos1 = curPositions[this.A];
   var pos2 = curPositions[this.B];
 
   var delta = { x: pos2.x - pos1.x, y: pos2.y - pos1.y };
-  
   var deltalength = Math.sqrt(delta.x * delta.x + delta.y * delta.y);
 
   var diff = (deltalength - this.restLength) / deltalength;
@@ -83,8 +92,8 @@ SpringConstraint.prototype.project = function (curPositions) {
   curPositions[this.A] = pos1;
   curPositions[this.B] = pos2;
 
-    if (deltalength < 200) {
-      return true;
+  if (deltalength < 200) {
+    return true;
   } else {
     return false;
   }
@@ -102,33 +111,47 @@ SpringConstraint.prototype.project = function (curPositions) {
  * @property {list} curPositions Current positions of the particles in system.
  * @property {list} oldPositions Positions of the particles in the previous step.
  * @property {list} posData Particle positoins ready to be sent to WebGL.
+ * @property {list} constraints List containing all the constraints. It might be
+ * of type {@link PinConstraint} or {@link SpringConstraint}.
  * @property {conData} conData The constraints will be drawn too, so this stores
  * an array of indices of the posData that will be drawn using an EBO so we
  * don't send duplicate data to WebGL. For example [2,3,4,8] means there are two
  * constraints that need to be drawin, one drawing a line between particle with
  * index 2 and 3 and the other one between 4 and 8.
+ * @property {object} gravity Has an x and y property to represent gravity. For
+ * this example this is what will populate the force accumulators list. So each
+ * element in the force accumulators list will have the same value as gravity.
  * @property {list} forceAccumulators List of forces represented as an object
  * with x and y properties that will be used by the verlet integration, before
  * the constraint projection, to advect the particles. Think of a force
  * accumulator as an external (not internal) force that acts on the particles.
- * @property {object} gravity Has an x and y property to represent gravity. For
- * this example this is what will populate the force accumulators list. So each
- * element in the force accumulators list will have the same value as gravity.
- * @property {list} constraint List containing all the constraints. It might be
- * of type {@link PinConstraint} or {@link SpringConstraint}.
+ * @property {number} num_springs Stores how many constraints are of type
+ * {@link SpringConstraint}, it's needed for drawing the right amount of lines.
+ * @property {object} clickCon Click events create constraints dynamically. Once
+ * a mouse down event is registered we look around the clicked point by a given
+ * radius and if some particles exists within the range we append them to the
+ * <code>constrained</code> property of this object as well as the
+ * <code>x</code> and <code>y</code> coordinates of the mouse. If the mouse is
+ * released the constraints will be removed from the system. And if the
+ * <code>breakable</code> flag is true the spring constraints will be deleted if
+ * they exceed 200px distance.
+ * @property {number} w Width of the canvas (on resize glViewport is called).
+ * @property {number} h Height of the canvas (on resize glViewport is called).
  */
 function ParticleSystem(canvasId) {
   this.NUM_ITERATIONS = 1;
   this.TIMESTEP = 1.0;
 
-  this.num_springs = 0;
   this.curPositions = [];
   this.oldPositions = [];
   this.posData = [];
-  this.conData = [];
-  this.forceAccumulators = [];
-  this.gravity = {x: 0, y: -3.0}; 
   this.constraints = [];
+  this.conData = [];
+
+  this.gravity = {x: 0, y: -3.0}; 
+  this.forceAccumulators = [];
+  
+  this.num_springs = 0;
 
   this.clickCon = {
     active: false,
@@ -145,7 +168,6 @@ function ParticleSystem(canvasId) {
   gl = canvas.getContext('webgl');
 
   this.initializeGL();
-
 
   // Events Setup
   var onResizeCallback = function () {
@@ -164,43 +186,40 @@ function ParticleSystem(canvasId) {
         c.x = this.clickCon.x;
         c.y = this.clickCon.y;
       }
-
     } 
   }
 
   var onMouseDownCallback = function (ev) {
+    this.clickCon.active = true;
+    this.clickCon.x = ev.clientX;
+    this.clickCon.y = window.innerHeight - ev.clientY;
 
-        this.clickCon.active = true;
-        this.clickCon.x = ev.clientX;
-        this.clickCon.y = window.innerHeight - ev.clientY;
-
-        this.curPositions.forEach(function(pos, idx) {
-          if (Math.abs(pos.x - this.clickCon.x) < this.clickCon.radius && Math.abs(pos.y - this.clickCon.y) < this.clickCon.radius) {
-            this.clickCon.constrained.push(idx);
-            this.constraints.push(new PinConstraint(idx, this.clickCon.x, this.clickCon.y));
-          }
-        }, this);
-        this.clickCon.breakable = true;
-        if (ev.button == 1) {
-          this.clickCon.breakable = false;
-        }
-
+    this.curPositions.forEach(function(pos, idx) {
+      if (Math.abs(pos.x - this.clickCon.x) < this.clickCon.radius &&
+          Math.abs(pos.y - this.clickCon.y) < this.clickCon.radius) {
+        this.clickCon.constrained.push(idx);
+        this.constraints.push(
+          new PinConstraint(idx, this.clickCon.x, this.clickCon.y)
+        );
+      }
+    }, this);
+    this.clickCon.breakable = true;
+    if (ev.button == 1) { // Middle click
+      this.clickCon.breakable = false;
+    }
   }
 
   var onMouseUpCallback = function(ev) {
     this.clickCon.active = false;
-
     for (var i = 0; i < this.clickCon.constrained.length; i++) {
       this.constraints.pop();
     }
-
     this.clickCon.constrained = [];
   }
 
   var onKeyDownCallback = function(ev) {
     switch(ev.keyCode) {
       case 38: // left arrow
-
         this.gravity.y += 0.1;
         break;
       case 40: // down arrow
@@ -208,14 +227,12 @@ function ParticleSystem(canvasId) {
         break;
       default:
         break;
-      
     }
   }
-
-  window.addEventListener('keydown', onKeyDownCallback.bind(this));
   canvas.addEventListener('mousemove', onMouseMoveCallback.bind(this));
   canvas.addEventListener('mousedown', onMouseDownCallback.bind(this));
   canvas.addEventListener('mouseup', onMouseUpCallback.bind(this));
+  window.addEventListener('keydown', onKeyDownCallback.bind(this));
   window.addEventListener('resize', onResizeCallback.bind(this));
 }
 
@@ -314,9 +331,10 @@ ParticleSystem.prototype.verlet = function () {
   }
 }
 
-/** All the constraints will be satisfied
+/**
+ * All the constraints will be satisfied by modifying the current positions.
  * @memberof ParticleSystem
- **/
+**/
 ParticleSystem.prototype.satisfyConstraints = function () {
   for (var it = 0; it < this.NUM_ITERATIONS; it++) {
     // Satisfy first constraint (box bounds)
@@ -326,16 +344,20 @@ ParticleSystem.prototype.satisfyConstraints = function () {
       pos.y = Math.min(Math.max(pos.y, 0), this.h);
     }
 
+    // Satisfy rest of the constraints (pin or spring)
     for (var i = 0; i < this.constraints.length; i++) { 
       var alive = this.constraints[i].project(this.curPositions);
       if (!alive && this.clickCon.breakable) {
+        // If constraint returns false means it should die (for example
+        // exceeding) the distance threshold. If the user has also pressed the
+        // left mouse button (so clickCon.breakable is true) we delete it.
         this.constraints.splice(i, 1);
       }
     }
   }
 }  
 
-/** Packs all data into single flat array and sends it to client
+/** Packs all data into single flat array and sends it to client WebGL.
  * @memberof ParticleSystem
  */
 ParticleSystem.prototype.sendDataToGL = function () { 
@@ -365,7 +387,10 @@ ParticleSystem.prototype.sendDataToGL = function () {
 }
 
 /**
- * Perform a substep
+ * This is basically the game loop. It accumulates the forces, then performs the
+ * Velvet integration, then project all the positions so they satisfy the
+ * constraints, then sends the data to the WebGL buffers so it's ready to be
+ * drawn.
  * @memberof ParticleSystem
  **/
 ParticleSystem.prototype.step = function () {
@@ -376,15 +401,19 @@ ParticleSystem.prototype.step = function () {
   this.draw();
 }
 
-// Main execution
+// =============================================================================
+// LET'S PLAY!
+// =============================================================================
 var ps = new ParticleSystem('screen');
 
+// This creates a grid of particles connected following the rule:
+//   1. If it is in the first row just connect to LEFT.
+//   2. If it is in the first column DON'T' connect to LEFT.
+//   3. If it is not in either first row or last row connect to LEFT and UP.
 var cWidth = 25;
 var cHeight = 20;
-
 var rows = 40;
 var cols = 40;
-
 var startX = (window.innerWidth / 2.0) - (rows * cWidth) / 2.0;
 
 for (var i = 0; i < rows; i++) { 
@@ -417,7 +446,4 @@ for (var i = 0; i < rows; i++) {
   }
 }
 
-
-
-
-setInterval(ps.step.bind(ps), 20);
+setInterval(ps.step.bind(ps), 20);  // Run the main loop.
